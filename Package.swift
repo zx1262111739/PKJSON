9a10a6d0e6df51de03f2d8d5421395f74b4075d6
azirkk <azirkk@outlook.com>
2024-05-15 12:08:40 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "PKJSON", targets: ["PKJSON"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("6.6.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
     ],
     targets: [
         .target(name: "PKJSON", dependencies: [
